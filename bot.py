f707f6bd6d6d6730419aef97c4e24e815ef77688
sanhik-hub <sanhikgangopadhyay08@gmail.com>
2025-01-25 12:37:40 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -144,3 +144,16 @@ def main():
 
 if __name__ == "__main__":
     main()
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+Thread(target=run).start()
